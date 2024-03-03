d9071ccfa1dc72c20c61499eb2ca528af55eb2f9
oferbudin <oferbudin@mail.tau.ac.il>
2024-03-03 21:09:53 +0200

    primery keys

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -20,11 +20,21 @@ class Column:
         self.converter_funtion = converter_funtion
 
 
+class ForeignKey:
+    def __init__(self, table, column_name, foreign_column_name = None):
+        self.column_name  = column_name
+        self.table = table
+        self.foreign_column_name = foreign_column_name if foreign_column_name else column_name
+        
+
 class Table:
-    def __init__(self, name: str, csv_path, columns: list[Column]):
+    def __init__(self, name: str, csv_path, columns: list[Column], primary_kies = None, foreign_keys: list[ForeignKey] = None):
         self.name = name
         self.columns = columns
         self.data_file_path = csv_path
+        self.primary_kies = primary_kies
+        self.foreign_keys = foreign_keys
+
     
     def get_records(self, data_file_path: str):
         data = load_data(data_file_path)
@@ -41,6 +51,12 @@ class Table:
         for column in self.columns:
             cmd += f"{column.name} {column.type},"
         cmd = cmd[:-1]
+        if self.primary_kies:
+            cmd += f",PRIMARY KEY ({','.join(self.primary_kies)}),"
+            cmd = cmd[:-1]
+        if self.foreign_keys:
+            for foreign_key in self.foreign_keys:
+                cmd += f",FOREIGN KEY ({foreign_key.column_name}) REFERENCES {foreign_key.table}({foreign_key.foreign_column_name})"
         cmd += ")"
         cursor.execute(cmd)
 
@@ -58,54 +74,13 @@ class DB:
 db = DB()
 db.add_table(
     Table(
-        "geners",
+        "genres",
         'genres.csv',
         [
             Column("id", "INT"),
             Column("name", "VARCHAR(255)")
-        ]
-    )
-)
-db.add_table(
-    Table(
-        "actors",
-        'actors.csv',
-        [
-            Column("id", "INT"),
-            Column("name", "VARCHAR(255)"),
-        ]
-    )
-)
-db.add_table(
-    Table(
-        "actor_movies",
-        'actor_movies.csv',
-        [
-            Column("movie_id", "INT"),
-            Column("actor_id", "INT"),
-        ]
-    )
-)
-db.add_table(
-    Table(
-        "crew_members",
-        'crew_members.csv',
-        [
-            Column("id", "INT"),
-            Column("name", "VARCHAR(255)"),
-            Column("job", "VARCHAR(255)"),
-        ]
-    )
-)
-db.add_table(
-    Table(
-        "crew_members_movies",
-        'crew_members_movies.csv',
-        [
-            Column("movie_id", "INT"),
-            Column("crew_id", "INT"),
-            Column("role", "VARCHAR(255)"),
-        ]
+        ],
+        primary_kies = ["id"]
     )
 )
 db.add_table(
@@ -119,8 +94,58 @@ db.add_table(
             Column("release_date", "DATE", lambda x: (datetime.strptime(x, "%d/%m/%Y") if '/' in x else datetime.strptime(x, "%Y-%m-%d").strftime("%Y-%m-%d"))),
             Column("runtime", "INT"),
             Column("original_language", "VARCHAR(255)"),
-            Column("gener", "INT"),
+            Column("genre", "INT"),
+        ],
+        primary_kies = ["id"],
+        # foreign_keys = [ForeignKey("genres", "id")]
+    )
+)
+db.add_table(
+    Table(
+        "actors",
+        'actors.csv',
+        [
+            Column("id", "INT "),
+            Column("name", "VARCHAR(255)"),
         ]
+    )
+)
+db.add_table(
+    Table(
+        "actor_movies",
+        'actor_movies.csv',
+        [
+            Column("movie_id", "INT"),
+            Column("actor_id", "INT"),
+        ],
+        primary_kies = ["movie_id", "actor_id"],
+        # foreign_keys = [ForeignKey("movies", "movie_id", "id"), ForeignKey("actors", "actor_id", "id")]
+        # foreign_keys = [ForeignKey("movies", "movie_id", "id")]
+
+    )
+)
+db.add_table(
+    Table(
+        "crew_members",
+        'crew_members.csv',
+        [
+            Column("id", "INT"),
+            Column("name", "VARCHAR(255)"),
+            Column("job", "VARCHAR(255)"),
+        ],
+        primary_kies = ["id"]
+    )
+)
+db.add_table(
+    Table(
+        "crew_members_movies",
+        'crew_members_movies.csv',
+        [
+            Column("movie_id", "INT"),
+            Column("crew_id", "INT"),
+        ],
+        primary_kies = ["movie_id", "crew_id"],
+        # foreign_keys = [ForeignKey("movies", "movie_id", "id"), ForeignKey("crew_members", "crew_id", "id")]
     )
 )
 db.add_table(
@@ -132,7 +157,9 @@ db.add_table(
             Column("popularity", "FLOAT"),
             Column("vote_average", "FLOAT"),
             Column("vote_count", "INT"),
-        ]
+        ],
+        primary_kies=["movie_id"],
+        # foreign_keys = [ForeignKey("movies", "movie_id", "id")]
     )
 )
 db.add_table(
@@ -143,7 +170,8 @@ db.add_table(
             Column("movie_id", "INT", key="id"),
             Column("budget", "INT"),
             Column("revenue", "INT"),
-        ]
+        ],
+        primary_kies=["movie_id"],
     )
 )
 
